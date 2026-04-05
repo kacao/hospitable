1cf4f4ba014f305a9d12f7cfee9e02ee1d690c5a
Khanh Cao <khanhncao@gmail.com>
2026-04-04 22:24:15 -0700

    0.3.2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export const VERSION = '0.3.1'
+export const VERSION = '0.3.2'
 
 export { HospitableClient } from './client'
 export type { HospitableClientConfig, ResourceCacheConfig } from './client'
